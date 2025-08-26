24d923bb3284a2135e3e332ff3156c5dd04f07e2
Siddharth Ganesan <siddharthganesan@gmail.com>
2025-08-25 17:34:21 -0700

    Update

--- apps/sim/db/migrations/0078_flimsy_monster_badoon.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "enterprise_copilot_api_keys" (
-	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
-	"name" text NOT NULL,
-	"api_key_encrypted" text NOT NULL,
-	"api_key_lookup" text NOT NULL
-);
---> statement-breakpoint
-CREATE INDEX "enterprise_copilot_api_keys_api_key_encrypted_hash_idx" ON "enterprise_copilot_api_keys" USING hash ("api_key_encrypted");--> statement-breakpoint
-CREATE INDEX "enterprise_copilot_api_keys_lookup_hash_idx" ON "enterprise_copilot_api_keys" USING hash ("api_key_lookup");
